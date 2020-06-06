e089eaa917ea57a57a9d6e1d02a231bc42f3662f
Legends <jovan27@gmx.de>
2020-06-07 00:20:31 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,6 @@
 // import lazy from "./lazy.js"
 
 export default function main() {
-   import("./lazy.js").then(mod => console.log(mod));
+   import("./lazy.js").then(mod => console.log(mod)); // comment out this one and comment in the other statements --> no error
     // console.log(lazy());
   }
