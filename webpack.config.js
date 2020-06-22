c5702fd7041e52a9a4958e2439f948c4e27fe4bf
Legends <jovan27@gmx.de>
2020-06-23 01:57:24 +0200

    make it work

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 export default {
   target: "node",
+  mode: "development",
   output: {
     filename: `js/[name].js`,
     chunkFilename: `js/[id]-chunk.js`,
     path: __dirname + '/dist'
-  }, 
+  },
 };
